d864a7fe8127dfcf6829f8079522de4c9bc4980d
Codi <514659285@qq.com>
2015-04-28 19:23:10 +0800

    test app compact theme.

---
+++ testtheme/src/main/java/com/codi/testtheme/MainActivity.java
@@ -0,0 +1,80 @@
+package com.codi.testtheme;
+
+import android.content.DialogInterface;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Drawable drawable = getResources().getDrawable(R.drawable.ic_launcher);
+
+        // Wrap the drawable so that future tinting calls work
+        // on pre-v21 devices. Always use the returned drawable.
+        drawable = DrawableCompat.wrap(drawable);
+
+        // We can now set a tint
+        DrawableCompat.setTint(drawable, Color.RED);
+        // ...or a tint list
+//        DrawableCompat.setTintList(drawable, myColorStateList);
+        // ...and a different tint mode
+        DrawableCompat.setTintMode(drawable, PorterDuff.Mode.CLEAR);
+
+        ((ImageView) findViewById(R.id.iv)).setImageDrawable(drawable);
+
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("hello").setMessage("are you ok?\n\nwhat are you doing...")
+                .setPositiveButton("Sure", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).create();
+        dialog.show();
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
